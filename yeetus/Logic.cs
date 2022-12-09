c5ce1a6181b44058eff0224d838dc2c707c2a10e
00erirun <00erirun@MS714105>
2022-12-09 18:01:19 +0100

    fistcomit

---
+++ yeetus/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yeetus
+{
+    internal class Logic
+    {
+        internal static int CalculateBron(int age)
+        {
+            var bdate = DateTime.Now.Year - age;
+            return bdate;
+        }
+    }
+}
---
+++ yeetus/Program.cs
@@ -0,0 +1,11 @@
+﻿using yeetus;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var perosn = new Person { Age = 69 };
+        var bday = Logic.CalculateBron(perosn.Age);
+        Console.WriteLine(bday);
+    }
+}
